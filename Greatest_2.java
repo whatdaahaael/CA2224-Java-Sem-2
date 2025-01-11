eaa3f43d1adbf7a7ee069f676b8981c4e1e9372e
Sujan <156099795+whatdaahaael@users.noreply.github.com>
2025-01-11 21:34:54 +0530

    Update Greatest_2.java

--- Greatest_2.java
+++ Greatest_2.java
@@ -6,6 +6,10 @@ public class Greatest_2 {
         System.out.println("Enter 2 numbers:");
         X = in.nextInt();
         Y = in.nextInt();
-        System.out.println((X>Y)?(X+" is Greater Than "+Y):(X+" is Not Greater Than "+Y));
+        if(X>Y){
+            System.out.println(X+" is Greater Than "+Y);
+        } else {
+            System.out.println(X+" is Not Greater Than "+Y);
+        }
     }
 }
