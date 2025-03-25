8b89e930ea27cda260ed72d6cde1d9c8ec3e9628
Sujan <156099795+whatdaahaael@users.noreply.github.com>
2025-03-25 23:13:23 +0530

    Update and rename CollegeData.c to GUI6FRAME.java

---
+++ GUI6FRAME.java
@@ -0,0 +1,69 @@
+/*
+ * Question 6:
+ * Program which creates a frame with two buttons father and mother.
+ * When we click the father button, the name of the father,
+ * his age and designation must appear.
+ * When we click the mother button, similar details of the mother also appear.
+ *
+ *
+ * 242BCA16
+ * Sujan
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI6FRAME extends Frame {
+    private Label detailsLabel;
+    private Button fatherButton, motherButton;
+
+    GUI6FRAME() {
+        // Create components
+        detailsLabel = new Label("Click a button to see details.");
+        fatherButton = new Button("Father");
+        motherButton = new Button("Mother");
+
+        // Set up listeners
+        addButtonClickListener();
+        addWindowCloseListener();
+
+        // Add components to frame
+        add(fatherButton);
+        add(motherButton);
+        add(detailsLabel);
+
+        // Frame properties
+        setSize(350, 200);
+        setTitle("GUI: Parent Details");
+        setLayout(new FlowLayout());
+        setVisible(true);
+    }
+
+    private void addButtonClickListener() {
+        fatherButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                detailsLabel.setText("Father: John, Age: 45, Designation: Engineer");
+            }
+        });
+
+        motherButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                detailsLabel.setText("Mother: Mary, Age: 42, Designation: Doctor");
+            }
+        });
+    }
+
+    private void addWindowCloseListener() {
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new GUI6FRAME();
+    }
+}
